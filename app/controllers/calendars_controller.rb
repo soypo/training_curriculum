377ad188c7cc72286ad0164a3885fabc36dc8d1e
soypo <136891264+soypo@users.noreply.github.com>
2023-09-07 18:53:09 +0900

    曜日が表示されないため修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -35,11 +34,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today/wday + x# wdayメソッドを用いて取得した数値
-      if wday_num >= 7#「wday_numが7以上の場合」という条件式
+      wday_num = Date.today.wday + x
+      if wday_num >= 7
         wday_num = wday_num -7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => (@week_days)}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
       @week_days.push(days)
     end
 
